2be8daf9d1206642c9dd173ed3a673fd08b1ecf2
Gabriel Sousa <90335814+GaBRiel4115@users.noreply.github.com>
2022-02-18 21:49:21 -0300

    Add files via upload

--- Aula01.sql
+++ Aula01.sql
@@ -36,13 +36,141 @@ from alunos
 where
 idade is not null -- idade não nulo
 AND
-nome= 'Rodrigo';
+nome = 'Rodrigo';
 
 -- filtrar ignorando registros duplicados
 select distinct nome, idade
 from alunos;
 
+-- ordenar os alunos por nome
+select * 
+from alunos
+order by nome asc;
+-- desc também
 
+
+
+-- selecionar os alunos com 
+-- idade acima de 21
+-- os dois mais velhos
+select *
+from alunos 
+where 
+idade > 21
+order by idade DESC
+limit 2;
+
+
+
+
+-- selecione todos os alunos que:
+alter table alunos drop column nota;
+alter table alunos
+add column nota int after idade;
+
+update alunos set nota = 95 where id  = 1;
+update alunos set nota = 87 where id  = 2;
+update alunos set nota = 50 where id  = 3;
+update alunos set nota = 60 where id  = 4;
+update alunos set nota = 152 where id = 5;
+update alunos set nota = 85 where id  = 6;
+update alunos set nota = 10 where id  = 7;
+update alunos set nota = 5 where id   = 8;
+
+-- selecione todos os alunos que:
+-- tenha nota maior ou igual a 70
+-- ordenação decrescente por idade 
+-- limitado a 3 alunos
+
+select *
+from alunos
+where nota >= 70
+order by idade DESC
+limit 3;
+
+select * from alunos;
+
+-- atualizar informações de um registro
+update alunos
+set idade = 100,  nascimento = '1922-02-17'
+where
+id = 5;
+
+-- select * from alunos
+-- where
+-- id = 5;
+
+
+-- excluir um registro
+delete from alunos
+where id = 5;
+
+-- operadores lógicos AND / OR
+
+-- selecione todos os registros que tenham o nome igual a 'André Neves'
+-- e idade igual a 100
+
+select * from alunos
+where
+nome  = 'Rodrigo'
+AND
+idade = 22;
+
+select * from alunos;
+
+-- selecione todos os registros que tenham o nome igual 'Rodrigo'
+-- ou idade igual a 22 anos 
+select * from alunos
+where
+nome  = 'Rodrigo'
+or
+idade = 22;
+
+-- Para saber o valor do auto_increment
+SELECT AUTO_INCREMENT FROM information_schema.tables
+WHERE table_name = 'alunos' AND table_schema = 'bd3';
+
+-- negacao
+-- selecionar todos os registros que não tenham 'Rodrigo' no nome
+select * from alunos
+where not nome = 'Rodrigo';
+
+-- funções no SGBD
+select * from alunos;
+-- contar as ocorrências total de idade
+select count(idade)
+from alunos;
+
+-- pegar a média das idades
+select AVG(idade)
+from alunos;
+
+-- soma 
+select SUM(idade)
+from alunos;
+
+
+
+-- menor idade
+select MIN(idade)
+from alunos;
+
+-- maior idade
+select MAX(idade)
+from alunos;
+
+
+
+-- procurar uma string dentro de um campo texto
+select * from alunos;
+-- 'Os pokemons salvaram o mundo'
+
+-- 'pokemon'
+select * from alunos
+where
+nome like '%Ro';
+
+-- caracter curinga %
 
 
 
